a90dff65b9adcbe9cdd88dd3f56b0c3c51fb45d5
Bapusaheb Patil <bapoo1998@gmail.com>
2017-10-06 21:54:35 +0530

    Added the dependency for ExoPlayer

--- app/src/main/java/bapspatil/captainchef/MainActivity.java
+++ app/src/main/java/bapspatil/captainchef/MainActivity.java
@@ -1,13 +1,21 @@
 package bapspatil.captainchef;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.hello_tv)
+    TextView mHelloTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
