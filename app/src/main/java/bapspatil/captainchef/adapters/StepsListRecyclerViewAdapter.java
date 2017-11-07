4c6392b9a5dbb896caf699b3df031ea126084128
Bapusaheb Patil <bapoo1998@gmail.com>
2017-11-07 11:24:50 +0530

    Working on thumbnails for recipe steps, made some CardView design changes

--- app/src/main/java/bapspatil/captainchef/adapters/StepsListRecyclerViewAdapter.java
+++ app/src/main/java/bapspatil/captainchef/adapters/StepsListRecyclerViewAdapter.java
@@ -1,11 +1,18 @@
 package bapspatil.captainchef.adapters;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.media.MediaMetadataRetriever;
+import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
@@ -22,6 +29,7 @@ public class StepsListRecyclerViewAdapter extends RecyclerView.Adapter<StepsList
     private Context mContext;
     private ArrayList<RecipeStep> mRecipeStepsList;
     private OnRecipeStepClickedListener mClickListener;
+    private Bitmap mBitmap;
 
     public StepsListRecyclerViewAdapter(Context context, ArrayList<RecipeStep> recipeStepArrayList, OnRecipeStepClickedListener clickedListener) {
         mContext = context;
@@ -40,6 +48,14 @@ public class StepsListRecyclerViewAdapter extends RecyclerView.Adapter<StepsList
         RecipeStep recipeStep = mRecipeStepsList.get(i);
         String recipeStepString = recipeStep.getStepId() + ". " + recipeStep.getShortInfo();
         stepsListViewHolder.mStepTextView.setText(recipeStepString);
+
+        (new LoadThumbnailsTask()).execute(recipeStep.getVideoUrl());
+        Glide.with(mContext)
+                .load(mBitmap)
+                .centerCrop()
+                .fallback(R.drawable.fallback_recipe_thumbnail)
+                .error(R.drawable.fallback_recipe_thumbnail)
+                .into(stepsListViewHolder.mStepImageView);
     }
 
     @Override
@@ -47,8 +63,9 @@ public class StepsListRecyclerViewAdapter extends RecyclerView.Adapter<StepsList
         return mRecipeStepsList.size();
     }
 
-    class StepsListViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    class StepsListViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         @BindView(R.id.step_item_tv) TextView mStepTextView;
+        @BindView(R.id.step_item_iv) ImageView mStepImageView;
 
         StepsListViewHolder(View itemView) {
             super(itemView);
@@ -58,7 +75,7 @@ public class StepsListRecyclerViewAdapter extends RecyclerView.Adapter<StepsList
 
         @Override
         public void onClick(View v) {
-            if(mClickListener != null) {
+            if (mClickListener != null) {
                 RecipeStep recipeStep = mRecipeStepsList.get(getAdapterPosition());
                 mClickListener.onRecipeStepClicked(recipeStep);
             }
@@ -67,5 +84,38 @@ public class StepsListRecyclerViewAdapter extends RecyclerView.Adapter<StepsList
 
     public interface OnRecipeStepClickedListener {
         void onRecipeStepClicked(RecipeStep recipeStep);
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class LoadThumbnailsTask extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            Bitmap bitmap = null;
+            String videoPath = strings[0];
+            MediaMetadataRetriever mediaMetadataRetriever = null;
+            try {
+                mediaMetadataRetriever = new MediaMetadataRetriever();
+                mediaMetadataRetriever.setDataSource(videoPath);
+                bitmap = mediaMetadataRetriever.getFrameAtTime();
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            } finally {
+                if (mediaMetadataRetriever != null)
+                    mediaMetadataRetriever.release();
+            }
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            mBitmap = bitmap;
+        }
     }
 }
